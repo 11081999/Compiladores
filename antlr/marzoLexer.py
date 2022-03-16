6190061813b62263cbaacb083e7c9b75459029f9
11081999 <robertoriverateran@gmail.com>
2022-03-15 18:26:48 -0600

    Fixed bugs
    s

--- antlr/marzoLexer.py
+++ antlr/marzoLexer.py
@@ -1,4 +1,4 @@
-# Generated from c:\Users\Roberto\Desktop\Compiladores\antlr\marzo.g4 by ANTLR 4.9.3
+# Generated from c:\Users\Roberto\Desktop\Compiladores\Compiladores\antlr\marzo.g4 by ANTLR 4.9.3
 from antlr4 import *
 from io import StringIO
 import sys
